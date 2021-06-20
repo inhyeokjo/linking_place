d5674ec10c2e4dbf0138816850473229bf8c9375
HeeHyeonKim <muke0822@naver.com>
2021-06-20 18:47:45 +0900

    구태희, 김희현 : 단위 테스트 (회원가입 로그인 수정)

--- se_team3_dir/seuser/tests.py
+++ se_team3_dir/seuser/tests.py
@@ -1,51 +1,48 @@
 import json
-# import bcrypt
+import unittest
 from django.test import TestCase
 from django.test import Client
 from .models import Seuser
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # 웹 어플리케이션의 기능을 테스트 합니다.
+
+info = { 'user_id' : 'rlagmlgus115',
+    'user_name' : '김희현',
+    'user_email' : 'rlagmlgus115@gmail.com',
+    'password' : 'redbull250',
+    'user_sex' : '남자',
+    'user_age' : 25,
+    'user_address' : '광주광역시 북구 첨단과기로 235',
+    'user_interest' : '보컬 트레이닝'
+    }
+
+client = Client()
 
 class Webapp_test(TestCase):
     def test_register(self):
         """
         회원가입 기능을 테스트 합니다.
         """
-        # user = get_user_model()
-        info = { 'user_id' : 'rlagmlgus115',
-            'user_name' : '김희현',
-            'user_email' : 'rlagmlgus115@gmail.com',
-            'password' : 'redbull250',
-            'user_sex' : '남자',
-            'user_age' : 25,
-            'user_address' : '광주광역시 북구 첨단과기로 235',
-            'user_interest' : '보컬 트레이닝'
-            }
 
-        # user = Seuser.objects.create(
-        #     user_id = 'rlagmlgus115',
-        #     user_name = '김희현',
-        #     user_email = 'rlagmlgus115@gmail.com',
-        #     password = 'redbull250',
-        #     user_sex = '남자',
-        #     user_age = 25,
-        #     user_address = '광주광역시 북구 첨단과기로 235',
-        #     user_interest = '보컬 트레이닝'
-        # )
-
-        client = Client()
-        response = client.post("/templates/login", json.dumps(info), content_type = 'application/json')
-        # self.assertEqual(response.status_code, 200)
+        response = client.post("/templetes/register", json.dumps(info), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
 
 
     def test_login(self):
         """
         로그인 기능을 테스트 합니다.`
         """
+        client.post("/templates/register", json.dumps(info), content_type = 'application/json')
         
+        test_login_data = {
+            'user_id' : 'rlagmlgus115',
+            'password' : 'redbull250',
+        }
 
-        return 1
+        response = client.post("/templetes/login/", json.dumps(test_login_data), content_type = 'application/json')
+        self.assertEqual(response.status_code, 200)
 
     def test_mv_category_page(self):
         """
