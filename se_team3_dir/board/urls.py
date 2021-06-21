93de987c622c44aac8a1934a3a297ceec4eb525a
Youngju Na <apercent1@naver.com>
2021-06-21 12:17:16 +0900

    나영주, 조인혁: 로그인/로그아웃, 카테고리별 화면, 준비중 페이지 구현

--- se_team3_dir/board/urls.py
+++ se_team3_dir/board/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.main_frame)
+    path("", views.main_frame),
+    path("studyroom", views.category_studyroom),
+    path("contents_not_ready", views.contents_not_ready),
+    path("post", views.post),
 ]
